fde78a4a1536df95dd70de0e0721ed6528dbec7a
ihossen016 <ihossen016@gmail.com>
2022-12-31 09:48:48 +0600

    classname changed

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,64 +7,64 @@ const Skills = () => {
         <section id="skills">
             <h5>The Skills I Have</h5>
             <h2>Skills</h2>
-            <div className="container experience__container">
-                <div className="experience__frontend">
+            <div className="container skill__container">
+                <div className="skill__frontend">
                     {/* <h3>Front-end Development</h3> */}
-                    <div className="experience__content">
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                    <div className="skill__content">
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>JavaScript</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>ReactJs</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>Redux-toolkit</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>RTK</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>Next Js</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>SASS/SCSS</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>Tailwind</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>Material UI</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>NodeJs</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>ExpressJs</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>MySQL</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>Firebase</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>MongoDB</h4>
                         </article>
-                        <article className="experience__details">
-                            <BsFillPatchCheckFill className="experience__details-icon" />
+                        <article className="skill__details">
+                            <BsFillPatchCheckFill className="skill__details-icon" />
                             <h4>WordPress</h4>
                         </article>
                     </div>
